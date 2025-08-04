aec50b1d250759b0294ec5b5e49e74fdfc044ffb
Veeresh77-31 <veereshbaitigeri143@gmail.com>
2025-08-04 11:24:03 +0530

    Changes-1

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
-// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
